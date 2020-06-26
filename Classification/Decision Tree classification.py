c8d74c2d0be05ab445b1b918aa06a7663f3a4f2d
Aryan Pandey <54200432+aryanp35@users.noreply.github.com>
2020-06-27 00:41:39 +0530

    Add files via upload

---
+++ Classification/Decision Tree classification.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+cd "d:\machineLearn\Machine_Learning_AZ_Template_Folder\Machine Learning A-Z Template Folder\Part 3 - Classification\Section 19 - Decision Tree Classification"
+
+
+# In[3]:
+
+
+pwd
+
+
+# In[4]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+# In[18]:
+
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, -1].values
+dataset
+
+
+# In[6]:
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+
+# In[8]:
+
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train1 = sc.fit_transform(X_train)
+X_test1 = sc.transform(X_test)
+# X_test
+
+
+# In[37]:
+
+
+from sklearn.tree import DecisionTreeClassifier 
+classifier=DecisionTreeClassifier(criterion='entropy')
+classifier.fit(X_train1,y_train)
+
+
+# In[38]:
+
+
+y_pred=classifier.predict(X_test1)
+y_pred
+
+
+# In[39]:
+
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test,y_pred)
+print(cm)
+accuracy_score(y_test,y_pred)
+
+
+# In[36]:
+
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() , stop = X_set[:, 0].max() , step = 1),
+                     np.arange(start = X_set[:, 1].min() , stop = X_set[:, 1].max() , step = 1))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Decision Tree (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+# In[13]:
+
+
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 1),
+                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 1))
+plt.contourf(X1, X2, classifier.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('K-NN (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+# In[16]:
+
+
+from sklearn.tree import export_graphviz
+from sklearn.externals.six import StringIO  
+from IPython.display import Image  
+import pydotplus
+
+
+# In[19]:
+
+
+dot_data = StringIO()
+feature_cols=['Age','Salary']
+export_graphviz(classifier, out_file=dot_data,  
+                filled=True, rounded=True,
+                special_characters=True,feature_names = feature_cols,class_names=['0','1'])
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+graph.write_png('hello.png')
+Image(graph.create_png())
+
